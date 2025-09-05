2da37f5a7ab466f2d7a13789d34074164f3df9de
Aryan Mishra <mishraaryan9204@gmail.com>
2025-09-05 08:26:30 +0530

    Transaction and deadlock

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -15,14 +15,19 @@ const (
 )
 
 var testQueries *Queries
+var testDB *sql.DB
+var testStore *Store
 
 func TestMain(m *testing.M) {
-	conn, err := sql.Open(dbDriver, dbSource)
+	var err error
+
+	testDB, err := sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("cannot connect to DB")
 	}
 
-	testQueries = New(conn)
+	testQueries = New(testDB)
+	testStore = NewStore(testDB)
 
 	os.Exit(m.Run())
 }
